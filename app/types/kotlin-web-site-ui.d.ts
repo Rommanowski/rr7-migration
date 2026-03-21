1e83ab3eb22771c7a3b8b5d5093079edc0ac5972
Rommanowski <jakub.romanowski17@gmail.com>
2026-03-21 15:41:13 +0100

    create a declaration file for kotlin web site ui

---
+++ app/types/kotlin-web-site-ui.d.ts
@@ -0,0 +1,9 @@
+declare module '@jetbrains/kotlin-web-site-ui/out/components/header/header.js' {
+    const Header: React.ComponentType<any>;
+    export default Header;
+}
+
+declare module '@jetbrains/kotlin-web-site-ui/out/components/footer/footer.js' {
+    const Footer: React.ComponentType<any>;
+    export default Footer;
+}
