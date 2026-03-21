25a5658d55e7c520ada5ebaa3d8d6c1089c08f73
Rommanowski <jakub.romanowski17@gmail.com>
2026-03-21 15:23:12 +0100

    migrate meta

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,18 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+export const meta: Route.MetaFunction = () => [
+    { title: "Kotlin Programming Language" },
+    { property: "og:title", content: "Kotlin Programming Language" },
+    { property: "og:type", content: "website" },
+    { property: "og:image", content: "https://kotlinlang.org/images/open-graph/general.png"},
+    { property: "og:url", content: "https://kotlinlang.org/"},
+    { property: "og:description", content: "Kotlin Programming Language"},
+    { property: "og:site_name", content: "Kotlin"},
+    { name: "twitter:card", content: "summary_large_image" },
+    { name: "twitter:site", content: "@kotlin" },
+    { name: "twitter:image:src", content: "https://kotlinlang.org/images/twitter/general.png" },
+];
 
 export default function Home() {
   return <Welcome />;
