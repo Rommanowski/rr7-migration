e23612014278b9e4835b309097c046f1f1b42eb4
Rommanowski <jakub.romanowski17@gmail.com>
2026-03-22 16:56:46 +0100

    delete reduntant files and add missing dependencies with Knip

--- app/types/kotlin-web-site-ui.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@jetbrains/kotlin-web-site-ui/out/components/header/header.js' {
-    const Header: React.ComponentType<any>;
-    export default Header;
-}
-
-declare module '@jetbrains/kotlin-web-site-ui/out/components/footer/footer.js' {
-    const Footer: React.ComponentType<any>;
-    export default Footer;
-}
